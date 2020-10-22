f1aa55347b50d6ad5210c2368e5bca612673bd98
stharmar <39895085+stharmar@users.noreply.github.com>
2020-10-22 15:21:52 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  We are the BEST meow!
+  We are the BEST!
   <!-- END -->
 
   </div>
